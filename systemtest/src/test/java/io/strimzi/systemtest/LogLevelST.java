462ad2eeef9599fbd9a2ec37e65a6f5b31e4cc0c
Jakub Stejskal <stejskinek@gmail.com>
2018-09-18 21:42:07 +0200

    fixup! NEW: ST for different log levels

--- systemtest/src/test/java/io/strimzi/systemtest/LogLevelST.java
+++ systemtest/src/test/java/io/strimzi/systemtest/LogLevelST.java
@@ -1,17 +1,13 @@
 package io.strimzi.systemtest;
 
 
-import io.strimzi.api.kafka.model.Kafka;
 import io.strimzi.test.ClusterOperator;
 import io.strimzi.test.Namespace;
 import io.strimzi.test.StrimziRunner;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.junit.Test;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.ValueSource;
 import org.junit.runner.RunWith;
-
 
 import static junit.framework.TestCase.assertTrue;
 
@@ -22,35 +18,86 @@ public class LogLevelST extends AbstractST {
     static final String NAMESPACE = "log-level-cluster-test";
     private static final Logger LOGGER = LogManager.getLogger(LogLevelST.class);
     private static final String TESTED_LOGGER = "kafka.root.logger.level";
-    private static final String LOG_LEVEL = "LOG_LEVEL";
     private static final String RESOURCE_TYPE = "statefulset";
     private static final String POD_NAME = kafkaClusterName(CLUSTER_NAME) + "-0";
 
     @Test
-    void testLogLevel(){
-        LOGGER.info("Running testLogLevel in namespace {}", NAMESPACE);
+    public void testLogLevelInfo() {
+        LOGGER.info("Running testLogLevelInfo in namespace {}", NAMESPACE);
+        String logLevel = "INFO";
+        createKafkaPods(logLevel);
+        assertTrue("Kafka's log level is set properly", checkKafkaLogLevel(logLevel));
+    }
 
-        resources().kafka(resources().defaultKafka(CLUSTER_NAME, 1)
-            .editSpec()
-                .editKafka().
-                    addToConfig(TESTED_LOGGER, LOG_LEVEL)
-                .endKafka()
-            .endSpec()
-        .build()).done();
+    @Test
+    public void testLogLevelError() {
+        LOGGER.info("Running testLogLevelError in namespace {}", NAMESPACE);
+        String logLevel = "ERROR";
+        createKafkaPods(logLevel);
+        assertTrue("Kafka's log level is set properly", checkKafkaLogLevel(logLevel));
+    }
 
-//        String logLevel = (String)kafka.getSpec().getKafka().getConfig().get(TESTED_LOGGER);
-//        boolean result = logLevel.equals(LOG_LEVEL);
+    @Test
+    public void testLogLevelWarn() {
+        LOGGER.info("Running testLogLevelWarn in namespace {}", NAMESPACE);
+        String logLevel = "WARN";
+        createKafkaPods(logLevel);
+        assertTrue("Kafka's log level is set properly", checkKafkaLogLevel(logLevel));
+    }
 
+    @Test
+    public void testLogLevelTrace() {
+        LOGGER.info("Running testLogLevelTrace in namespace {}", NAMESPACE);
+        String logLevel = "TRACE";
+        createKafkaPods(logLevel);
+        assertTrue("Kafka's log level is set properly", checkKafkaLogLevel(logLevel));
+    }
+
+    @Test
+    public void testLogLevelDebug() {
+        LOGGER.info("Running testLogLevelDebug in namespace {}", NAMESPACE);
+        String logLevel = "DEBUG";
+        createKafkaPods(logLevel);
+        assertTrue("Kafka's log level is set properly", checkKafkaLogLevel(logLevel));
+    }
+
+    @Test
+    public void testLogLevelFatal() {
+        LOGGER.info("Running testLogLevelFatal in namespace {}", NAMESPACE);
+        String logLevel = "FATAL";
+        createKafkaPods(logLevel);
+        assertTrue("Kafka's log level is set properly", checkKafkaLogLevel(logLevel));
+    }
+
+    @Test
+    public void testLogLevelOff() {
+        LOGGER.info("Running testLogLevelOff in namespace {}", NAMESPACE);
+        String logLevel = "OFF";
+        createKafkaPods(logLevel);
+        assertTrue("Kafka's log level is set properly", checkKafkaLogLevel(logLevel));
+    }
+
+    private boolean checkKafkaLogLevel(String logLevel) {
+        LOGGER.info("Check log level setting. Expected: {}", logLevel);
         String kafkaPodLog = kubeClient.logs(POD_NAME, "kafka");
-
-        boolean result = kafkaPodLog.contains("level="+LOG_LEVEL);
-
+        boolean result = kafkaPodLog.contains("level=" + logLevel);
 
         if(!result) {
-            kafkaPodLog = kubeClient.searchInLog(RESOURCE_TYPE, kafkaClusterName(CLUSTER_NAME), 600, LOG_LEVEL);
+            kafkaPodLog = kubeClient.searchInLog(RESOURCE_TYPE, kafkaClusterName(CLUSTER_NAME), 600, "ERROR");
             result = kafkaPodLog.isEmpty();
         }
 
-        assertTrue("Kafka's log level is set properly", result);
+        return result;
+    }
+
+    private void createKafkaPods(String logLevel) {
+        LOGGER.info("Create kafka in {} for testing logger: {}={}", CLUSTER_NAME, TESTED_LOGGER, logLevel);
+        resources().kafka(resources().defaultKafka(CLUSTER_NAME, 1)
+                .editSpec()
+                .editKafka().
+                        addToConfig(TESTED_LOGGER, logLevel)
+                .endKafka()
+                .endSpec()
+                .build()).done();
     }
 }
